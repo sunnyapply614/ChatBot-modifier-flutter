d5fb0dad6236540077a88fbc980fbcf61a943d6e
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-04 05:20:11 +0300

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,4 +1,5 @@
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // const urlMongodb = process.env.URL_MONGODB
 const urlMongodb = "mongodb+srv://root2:root123@cluster0.nk84uyl.mongodb.net/";
