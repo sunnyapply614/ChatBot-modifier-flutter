1ceea84bf041699f89ba139159417a9c2294dc82
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-04 05:16:49 +0300

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,19 +4,6 @@ require("dotenv").config();
 // const urlMongodb = process.env.URL_MONGODB
 const urlMongodb = "mongodb+srv://root2:root123@cluster0.nk84uyl.mongodb.net/";
 
-const connectDB = () => {
-  mongoose
-    .set("strictQuery", false)
-    .connect(urlMongodb, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((res) => {
-      console.log("Connect successfully!");
-    })
-    .catch((error) => {
-      console.log("Connect failure!");
-    });
-};
+
 
 module.exports = connectDB;
