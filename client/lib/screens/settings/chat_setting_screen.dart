4a534714ae3c64dcb4a3ad0b131c849d334c09ce
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-14 03:19:56 +0300

    Update chat_setting_screen.dart

--- client/lib/screens/settings/chat_setting_screen.dart
+++ client/lib/screens/settings/chat_setting_screen.dart
@@ -6,7 +6,8 @@ import 'package:flutter/material.dart';
 class ChatSettingScreen extends StatefulWidget {
   const ChatSettingScreen({super.key});
 
-
+  @override
+  State<ChatSettingScreen> createState() => _ChatSettingScreenState();
 }
 
 class _ChatSettingScreenState extends State<ChatSettingScreen> {
