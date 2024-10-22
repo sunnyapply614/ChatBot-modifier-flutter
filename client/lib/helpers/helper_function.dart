98329c01ac0ca8c2d6525f7531649f917d27e8a7
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-22 03:06:02 +0300

    Update helper_function.dart

--- client/lib/helpers/helper_function.dart
+++ client/lib/helpers/helper_function.dart
@@ -9,7 +9,15 @@ void nextScreen(context, page) {
   );
 }
 
-
+void showSnackBar(context, color, message) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message, style: const TextStyle(fontSize: 14)),
+      backgroundColor: color,
+      duration: const Duration(seconds: 2),
+    ),
+  );
+}
 
 Future<dynamic> showModalBottom(
   BuildContext context, {
