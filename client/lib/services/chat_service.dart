94460ad88794786695f398ac085ddf672282522e
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-31 03:07:03 +0200

    Update chat_service.dart

--- client/lib/services/chat_service.dart
+++ client/lib/services/chat_service.dart
@@ -3,17 +3,7 @@ import 'package:client/services/config.dart';
 import 'package:http/http.dart' as http;
 
 class ChatService {
-  static fetchChats(token) async {
-    final response = await http.get(
-      Uri.parse(chatUrl),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-    );
-    return response;
-  }
+
 
   static createChat(userId, token) async {
     final response = await http.post(
