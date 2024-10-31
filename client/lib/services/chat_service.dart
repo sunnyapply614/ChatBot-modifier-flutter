bc65f296507cc7eec508d5e4dc27576f00bf464f
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-31 03:07:33 +0200

    Update chat_service.dart

--- client/lib/services/chat_service.dart
+++ client/lib/services/chat_service.dart
@@ -15,5 +15,16 @@ class ChatService {
     return response;
   }
 
-
+  static createChat(userId, token) async {
+    final response = await http.post(
+      Uri.parse(chatUrl),
+      headers: {
+        'content-type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode({"userId": userId}),
+    );
+    return response;
+  }
 }
