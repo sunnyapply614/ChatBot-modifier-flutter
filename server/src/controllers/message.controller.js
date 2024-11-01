ab53fa742c02f30b4bae41cfbdd8f76451a966e1
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-01 16:14:55 +0200

    Update message.controller.js

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -18,10 +18,7 @@ const messageController = {
     sendMessage: async (req, res) => {
         const { content, chatId } = req.body
 
-        if (!content || !chatId) {
-            console.log("Invalid data passed into request")
-            return res.sendStatus(400)
-        }
+
 
         var newMessage = {
             sender: req.user._id,
