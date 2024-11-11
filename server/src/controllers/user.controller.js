9f7e17f9a46215411962beb103fb57e59a90c49d
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-11 13:26:13 +0200

    Update user.controller.js

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -2,7 +2,13 @@ const userModel = require('../models/user.model')
 const UserService = require('../services/user.service')
 
 const userController = {
-
+    handleRegister: async (req, res, next) => {
+        await UserService.registerUser(req.body)
+            .then(() => {
+                res.json({ status: true, success: 'User Registered Successfully' })
+            })
+            .catch(next)
+    },
 
     handleLogin: async (req, res, next) => {
         const { email, password } = req.body
