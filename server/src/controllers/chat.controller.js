8c6d1efce80c0e89b4c2d02e4a1e1aafe5a55099
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-11 13:22:52 +0200

    Update chat.controller.js

--- server/src/controllers/chat.controller.js
+++ server/src/controllers/chat.controller.js
@@ -47,7 +47,26 @@ const chatController = {
       }
     }
   },
-
+  fetchChats: async (req, res) => {
+    try {
+      chatModel
+        .find({ users: { $elemMatch: { $eq: req.user._id } } })
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password")
+        .populate("latestMessage")
+        .sort({ updatedAt: -1 })
+        .then(async (results) => {
+          results = await userModel.populate(results, {
+            path: "latestMessage.sender",
+            select: "name image email",
+          });
+          res.status(200).send(results);
+        });
+    } catch (error) {
+      res.status(400);
+      throw new Error(error.message);
+    }
+  },
 
 };
 
